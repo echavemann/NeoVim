3bf49f6aca7e700543e044f2a5769a75f31417ef
Ethan Havemann <echavemann@gmail.com>
2024-02-02 09:22:11 -0600

    Fixing LSP Autocomplete

--- lua/echavemann/lspconfig.lua
+++ lua/echavemann/lspconfig.lua
@@ -1,2 +1,52 @@
-local M = { "nvim-lspconfig" }
+local M = {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+	},
+}
+
+M.config = function()
+	local lspconfig = require("lspconfig")
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+	local on_attach = function()
+		local bufopts = { noremap = true, silent = true, buffer = 0 }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, bufopts)
+		vim.keymap.set("n", "<space>df", vim.diagnostic.goto_next, bufopts)
+	end
+
+	local servers = {
+		"lua_ls",
+		"pyright",
+        "clangd"
+	}
+
+	-- setup servers
+	for _, lsp in pairs(servers) do
+		lspconfig[lsp].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim", "LUASNIP_ENV" },
+						-- LUASNIP_ENV is a custom variable declared in `lua/plugins/luasnip.lua`
+					},
+				},
+				gopls = gopls_settings,
+			},
+		})
+	end
+end
 return M
--- lua/echavemann/luasnip.lua
+++ lua/echavemann/luasnip.lua
@@ -1,5 +1,36 @@
 local M = {
-  "saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
 }
+
+M.config = function()
+	local ls = require("luasnip")
+
+	-- global variable
+	LUASNIP_ENV = {
+		s = require("luasnip.nodes.snippet").S,
+		i = require("luasnip.nodes.insertNode").I,
+		fmt = require("luasnip.extras.fmt").fmt,
+		parse = require("luasnip.util.parser").parse_snippet,
+		p = require("luasnip.extras").partial,
+	}
+
+	ls.config.set_config({
+		history = true,
+		updateevents = "TextChanged,TextChangedI",
+		enable_autosnippets = true,
+	})
+
+
+	vim.keymap.set({ "i", "s" }, "<a-k>", function()
+		if ls.jumpable(1) then
+			ls.jump(1)
+		end
+	end)
+	vim.keymap.set({ "i", "s" }, "<a-j>", function()
+		if ls.jumpable(-1) then
+			ls.jump(-1)
+		end
+	end)
+end
 
 return M
