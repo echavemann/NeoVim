5c6e12d70616436d1e16c65ac040f6cc54417536
Ethan Havemann <echavemann@gmail.com>
2024-02-25 08:56:36 -0600

    Add Rust Analyzer to Mason Config

--- lua/echavemann/mason.lua
+++ lua/echavemann/mason.lua
@@ -12,7 +12,7 @@ local M = {
 function M.config()
   require("mason").setup(settings)
   require("mason-lspconfig").setup {
-    ensure_installed = {"pyright","cmake","dockerls","clangd","jsonls","pylsp", "terraformls", "taplo", "bashls"},
+    ensure_installed = {"pyright","cmake","dockerls","clangd","jsonls","pylsp", "terraformls", "taplo", "bashls", "rust-analyzer"},
     automatic_installation = true,
   }
 end
