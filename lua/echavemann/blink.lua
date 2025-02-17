48ec63bd2c0ca1579d6f02fe8005bf624eec9c41
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2025-02-17 17:22:56 -0600

    pin blink to 0.11

--- lua/echavemann/blink.lua
+++ lua/echavemann/blink.lua
@@ -5,7 +5,7 @@ local M = {
   dependencies = 'rafamadriz/friendly-snippets',
 
   -- use a release tag to download pre-built binaries
-  version = 'v0.*',
+  version = 'v0.11',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
